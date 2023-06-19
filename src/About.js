f6f20c672c8cfea3cc40a420986c8fc2005114e4
Bishal Chansi <chansibishal55@gmail.com>
2023-06-19 11:25:29 +0545

    Feature section

--- src/About.js
+++ src/About.js
@@ -1,13 +1,17 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
 import Footer1 from './components/Footer1'
+import { useProductContext } from "./context/Productcontex";
 const About = () => {
+  const { myName } = useProductContext();
+
   const data = {
     name: "Ashish SuperMart.    Find Happiness With Us. Happy shopping!",
   };
 
   return(
    <>
+    {myName}
   <HeroSection myData={data} />;
   <Footer1 />
   </>
